17967e88ad54ea853abf0d7a9652c0dbe02f1901
silviabrochet <73230658+silviabrochet@users.noreply.github.com>
2021-02-10 15:02:50 +0100

    Add files via upload

---
+++ sortmerna_invivo.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "SB_01"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_01_L7_R1_001_trimmed.fastq -fastx -aligned "SB_01_L7_R1_001_rRNA" -other "SB_01_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_02"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_02_L7_R1_001_trimmed.fastq -fastx -aligned "SB_02_L7_R1_001_rRNA" -other "SB_02_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_03"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_03_L7_R1_001_trimmed.fastq -fastx -aligned "SB_03_L7_R1_001_rRNA" -other "SB_03_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_04"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_04_L7_R1_001_trimmed.fastq -fastx -aligned "SB_04_L7_R1_001_rRNA" -other "SB_04_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_05"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_05_L7_R1_001_trimmed.fastq -fastx -aligned "SB_05_L7_R1_001_rRNA" -other "SB_05_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_06"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_06_L7_R1_001_trimmed.fastq -fastx -aligned "SB_06_L7_R1_001_rRNA" -other "SB_06_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_07"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_07_L7_R1_001_trimmed.fastq -fastx -aligned "SB_07_L7_R1_001_rRNA" -other "SB_07_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_08"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_08_L7_R1_001_trimmed.fastq -fastx -aligned "SB_08_L7_R1_001_rRNA" -other "SB_08_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_09"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_09_L7_R1_001_trimmed.fastq -fastx -aligned "SB_09_L7_R1_001_rRNA" -other "SB_09_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_10"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_10_L7_R1_001_trimmed.fastq -fastx -aligned "SB_10_L7_R1_001_rRNA" -other "SB_10_L7_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_01_r2"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_01_r2_L3_R1_001_trimmed.fastq -fastx -aligned "SB_01_r2_L3_R1_001_rRNA" -other "SB_01_r2_L3_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_03_r2"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_03_r2_L3_R1_001_trimmed.fastq -fastx -aligned "SB_03_r2_L3_R1_001_rRNA" -other "SB_03_r2_L3_R1_001_non-rRNA" -threads 15 -v
+
+echo "SB_04_r2"
+
+sortmerna -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-16s-id90.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-bac-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-16s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-arc-23s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-18s-id95.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/silva-euk-28s-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5s-database-id98.fasta -ref /home/silvia/invivo_RNAseq/fastq/rRNA_databases/rfam-5.8s-database-id98.fasta -reads SB_04_r2_L3_R1_001_trimmed.fastq -fastx -aligned "SB_04_r2_L3_R1_001_rRNA" -other "SB_04_r2_L3_R1_001_non-rRNA" -threads 15 -v
